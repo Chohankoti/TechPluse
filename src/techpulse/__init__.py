65bf8f15304843433d31354f4d57dd4b297b2573
chohan <chohankoti333@gmail.com>
2026-09-06 13:51:59 +0530

    chore: tested jsonc parser

--- src/techpulse/__init__.py
+++ src/techpulse/__init__.py
@@ -1,25 +1,31 @@
 import os
 from dotenv import load_dotenv
 from tinyfish import TinyFish
+from jsonc_parser.parser import JsoncParser
 
 load_dotenv()
 
+def get_new_post_ids() -> list[int]:
+    previous_post_ids = JsoncParser().parse_file("src/data/content_state.jsonc")["previous_post_ids"]
+    print(previous_post_ids)
 
-def main() -> None:
-    client = TinyFish()
-
-    urls = ["https://claude.com/check-files"]
-
+def get_url_content(url: str) -> str:
     try: 
-        fetched = client.fetch.get_contents(urls=urls, format="markdown")
+        client = TinyFish()
+        fetched = client.fetch.get_contents(urls=[url], format="markdown")
+        return fetched.results[0].text
     except Exception as e:  
         print(e)
-        return
+        return ""
 
-    content = fetched.results[0].text
 
-    with open("url_content", "w", encoding="utf-8") as f:
-        f.write(content)
+def main() -> None:
+    get_new_post_ids()
+    # print(get_url_content("https://claude.com/check-files"))
+
+
+   
+
 
 
 
